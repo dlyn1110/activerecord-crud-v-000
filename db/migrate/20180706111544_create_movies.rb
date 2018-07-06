4bf0a76394d3350f3e619bd659ff25b8dd09e560
Dani Lyn <dlyn1110@gmail.com>
2018-07-06 11:18:12 +0000

    automatically backed up by learn

--- db/migrate/20180706111544_create_movies.rb
+++ db/migrate/20180706111544_create_movies.rb
@@ -1,6 +1,6 @@
 class CreateMovies < ActiveRecord::Migration
   def change
-    create_table :costume_stores do |t|
+    create_table :movies do |t|
       t.string :title
       t.string :release_date
       t.integer :director
