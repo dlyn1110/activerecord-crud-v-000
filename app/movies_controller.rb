3f85e61f658570f459a954e1cdbcec8d6bf0728c
Dani Lyn <dlyn1110@gmail.com>
2018-07-06 12:05:48 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -17,7 +17,7 @@ def can_be_created_with_a_hash_of_attributes
       release_date: 1973,
       director: "George Roy Hill",
       lead: "Paul Newman",
-      in_theaters: false
+      in_theatres: false
   }
   movie = Movie.create(attributes)
 end
