41df0adefdc8a5e90b723540f8f41af121519779
Dani Lyn <dlyn1110@gmail.com>
2018-07-06 11:20:01 +0000

    automatically backed up by learn

--- db/migrate/20180706111544_create_movies.rb
+++ db/migrate/20180706111544_create_movies.rb
@@ -5,7 +5,7 @@ class CreateMovies < ActiveRecord::Migration
       t.datetime :release_date
       t.string :director
       t.string :lead
-      t.string :theatres
+      t.boolean :theatres
      end
     end
   end
