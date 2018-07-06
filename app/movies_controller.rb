413d6f3fe1b770de9162cd76723729af3088c4cd
Dani Lyn <dlyn1110@gmail.com>
2018-07-06 12:09:29 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,6 +20,8 @@ def can_be_created_with_a_hash_of_attributes
       in_theatres: false
   }
   movie = Movie.create(attributes)
+  movie.save
+  movie
 end
 
 def can_be_created_in_a_block(args = {title: 'Home Alone', release_date: 1990})
